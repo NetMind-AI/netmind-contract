239963f6f5f854ee03afc7abd98209f03cb68427
AronJeames <zm@protagonist-us.com>
2024-02-18 16:38:57 +0800

    update airdrop

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,43 +1,35 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 interface  IERC20 {
-      function transfer(address to, uint256 amount) external returns(bool);
-      function balanceOf(address user) external returns(uint256);
+      function transferFrom(address from, address to, uint256 amount) external returns(bool);
 }
 
 contract Airdrop {
       address public nmt;
+      address public bank;
       address public sender;
 
       modifier Onlysender(){
             require(msg.sender == sender, "Airdrop: Only sender");
             _;
       }
-      constructor(address _nmt,address _sender) {
+      constructor(address _nmt,address _bank) {
             nmt = _nmt;
-            sender = _sender;
+            bank = _bank;
+            sender = msg.sender;
       }
 
       function airdrop(address[] memory users, uint256[] memory amts) public Onlysender {
+            require(users.length == amts.length, "Airdrop: invalid length of users and amts");
             for (uint256 i = 0; i < users.length; i++){
-                  IERC20(nmt).transfer(users[i], amts[i]);
+                  IERC20(nmt).transferFrom(bank, users[i], amts[i]);
             } 
       }
 
       function airdrop(address[] memory users, uint256 amt) public Onlysender {
             for (uint256 i = 0; i < users.length; i++){
-                  IERC20(nmt).transfer(users[i], amt);
+                  IERC20(nmt).transferFrom(bank, users[i], amt);
             }
-      }
-
-      function withdrewAndDestruct(address to) public Onlysender {
-            //withdrew 
-            uint256 balance = IERC20(nmt).balanceOf(address(this));
-            IERC20(nmt).transfer(to, balance);
-
-            //destruct
-            //selfdestruct(to);
       }
 }
