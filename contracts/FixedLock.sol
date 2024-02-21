4619c94f5dd0f44976075c69a98716184a8e2d57
AronJeames <zm@protagonist-us.com>
2024-02-21 17:52:50 +0800

    update FixedLock contract

--- contracts/FixedLock.sol
+++ contracts/FixedLock.sol
@@ -36,73 +36,9 @@ abstract contract Initializable {
     }
 }
 
-contract Ownable is Initializable{
-    address private _owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    function __Ownable_init_unchained() internal initializer {
-        address msgSender = msg.sender;
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return msg.sender == _owner;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-contract FixedLock is Initializable, Ownable {
+contract FixedLock is Initializable {
     uint256 public startTime;
     uint256 public endTime;
     uint256 public deadLockDuration;
@@ -154,8 +90,6 @@ contract FixedLock is Initializable, Ownable {
         deadLockDuration = _lockDuration;
         rewardPropotion = _rewardPropotion;
         rewardDelay = _rewardDelay;
-
-        __Ownable_init_unchained();
     }
 
     function getLocks(address guy) public view returns(uint256[] memory){
