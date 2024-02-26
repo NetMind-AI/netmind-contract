f5e6de6b37ee58520e516d2c81755953f8915b8b
AronJeames <zm@protagonist-us.com>
2024-02-26 17:27:00 +0800

    update

--- contracts/FixedLock.sol
+++ contracts/FixedLock.sol
@@ -36,116 +36,60 @@ abstract contract Initializable {
     }
 }
 
-contract Ownable is Initializable{
-    address private _owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    function __Ownable_init_unchained() internal initializer {
-        address msgSender = msg.sender;
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return msg.sender == _owner;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-interface IERC20 {
-    function balanceOf(address user) external returns(uint256);
-    function transfer(address to, uint256 amt) external returns(bool);
-    function transferFrom(address from, address to, uint256 amt) external returns(bool);
-}
-
-contract FixedLock is Initializable, Ownable {
-    address public nmt;
+contract FixedLock is Initializable {
     uint256 public startTime;
     uint256 public endTime;
-    uint256 public lockDuration;
+    uint256 public deadLockDuration;
+    uint256 public releaseTimes;
+    uint256 public releasePeriod;
+    uint256 public totalLocked;
+
     uint256 public rewardPropotion;
     uint256 public rewardDelay;
 
-    uint256 public totalLocked;
-
+   
     struct LockInfo{
         address owner;
-        uint256 amount;
-        uint256 lockTime;
-        uint256 unlockTime;
+        uint256 locked;   
+        uint256 lockTime;                          
+        uint256 unlocked;
         uint256 rewardsEarned;
-        uint256 rewardsClaimTime;
     }
 
     uint256 public lockId;
-    mapping(uint256 => LockInfo) public lockInfo;   //lockId => lockInfo
+    mapping(uint256 => LockInfo) public lockInfo;   //lockId  => lockInfo
     mapping(address => uint256[]) private locks;     //owner  => lockId[]
 
     event Lock(uint256 indexed id, address indexed owner, uint256 amt);
     event Unlock(uint256 indexed id, address indexed owner, uint256 amt);
-    event Claim(uint256 indexed id, address indexed owner, uint256 amt);
+    event ClaimReward(uint256 indexed id, address indexed owner, uint256 amt);
+
+    modifier notContract() {
+        require((!_isContract(msg.sender)) && (msg.sender == tx.origin), "contract not allowed");
+        _;
+    }
+
+    function _isContract(address addr) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
+        }
+        return size > 0;
+    }
 
     constructor(){_disableInitializers();}
     
-    function init(address _nmt, uint256 _strat, uint256 _end, uint256 _lockDuration, uint256 _rewardPropotion, uint256 _rewardDelay) public initializer{
-        nmt = _nmt;
-        startTime = _strat;
-        endTime = _end;
-        lockDuration = _lockDuration;
+    function init(uint256 _endTime, uint256 _lockDuration, uint256 _rewardPropotion, uint256 _rewardDelay, bool _isMainNet) public initializer{
+        startTime = block.timestamp;
+        endTime = _endTime;
+        releaseTimes = 8;
+        releasePeriod = _isMainNet? 365 days: 1 minutes;
+
+        deadLockDuration = _lockDuration;
         rewardPropotion = _rewardPropotion;
         rewardDelay = _rewardDelay;
-
-        __Ownable_init_unchained();
     }
 
     function getLocks(address guy) public view returns(uint256[] memory){
@@ -162,62 +106,80 @@ contract FixedLock is Initializable, Ownable {
         return ls;
     }
 
-    function lock(uint256 amt) public returns(uint256 id){
+    function lock(uint256 amt) public payable notContract returns(uint256 id){
+        amt = amt * 1e18;
         require(block.timestamp > startTime, "activit not start");
         require(block.timestamp < endTime, "activit end");
-
-        //recever token 
-        IERC20(nmt).transferFrom(msg.sender, address(this), amt);
-
+        require(msg.value == amt , "invaild NMT value");
+        
         //save LockInfo
         totalLocked += amt;
-        lockInfo[++lockId] = LockInfo(msg.sender, amt, block.timestamp, 0, 0, 0);
+        lockInfo[++lockId] = LockInfo(msg.sender, amt, block.timestamp, 0, 0);
         locks[msg.sender].push(lockId);
 
         emit Lock(lockId, msg.sender, amt);
-
         return lockId;
     }
 
-    function unlock(uint256 id) public {
-        LockInfo storage lf = lockInfo[id];
-        require(lf.amount > 0, "lockId unexsit");
-        require(lf.unlockTime == 0, "already unlock");
-        require(lf.owner == msg.sender, "only owner can call");
-        require(block.timestamp > lf.lockTime + lockDuration, "unarrived unlock time");
+    function unlockable(uint256 id) public view returns (uint256){
+        LockInfo memory lf = lockInfo[id];
+        uint256 locked_t = block.timestamp - lf.lockTime;
 
-        lf.unlockTime = block.timestamp;
+        if (lf.locked == 0 || locked_t < deadLockDuration || lf.locked == lf.unlocked){
+            return 0;
+        }else {
+            uint256 _releasedLocked = locked_t - deadLockDuration;
+            uint256 _releasedTimes;
+            for (uint8 i=0; i< releaseTimes; i++){
+                if (_releasedLocked > i * releasePeriod) _releasedTimes++;
+            }
 
-        //release nmt
-        totalLocked -= lf.amount;
-        IERC20(nmt).transfer(lf.owner, lf.amount);
-
-        emit Unlock(lockId, msg.sender, lf.amount);
+            //released
+            uint256 released = lf.locked * _releasedTimes / releaseTimes;
+            return released - lf.unlocked;
+        }
     }
 
-    function claim(uint256 id) public {
+    function unlock(uint256 id, uint256 amt) public notContract{
+        amt = amt * 1e18;
         LockInfo storage lf = lockInfo[id];
-        require(lf.amount > 0, "lockId unexsit");
+        require(lf.locked > 0, "lockId unexsit");
         require(lf.owner == msg.sender, "only owner can call");
-        require(lf.rewardsClaimTime == 0, "already claim");
+        require(block.timestamp > lf.lockTime + deadLockDuration, "deadlocking");
+
+        //unlockable
+        uint256 _unlockable =  unlockable(id);
+        require(amt <= _unlockable && _unlockable <= lf.locked, "out of unlockable");
+
+        //release
+        totalLocked -= amt;
+        lf.unlocked += amt;
+        payable(msg.sender).transfer(amt);
+
+        emit Unlock(lockId, msg.sender, amt);
+    }
+    
+    function checkReward(uint256 id) public view returns(uint256){
+        LockInfo memory lf = lockInfo[id];
+        if (block.timestamp < lf.lockTime + rewardDelay || lf.rewardsEarned > 0){
+            return 0;
+        }
+        return lf.locked * rewardPropotion / 1000;
+    }
+
+    function claimReward(uint256 id) public notContract{
+        LockInfo storage lf = lockInfo[id];
+        require(lf.locked > 0, "lockId unexsit");
+        require(lf.owner == msg.sender, "only owner can call");
+        require(lf.rewardsEarned == 0, "already claim");
         require(block.timestamp > lf.lockTime + rewardDelay, "unarrived claim time");
 
-        if(lf.unlockTime == 0){
-            totalLocked -= lf.amount;
-            lf.unlockTime = block.timestamp;
-            IERC20(nmt).transfer(lf.owner, lf.amount);
-            emit Unlock(lockId, msg.sender, lf.amount);
-        }
-
         //claim reward
-        uint256 reward = lf.amount * rewardPropotion / 1000;
-        uint256 balance = IERC20(nmt).balanceOf(address(this));
-        require(balance - reward >= totalLocked, "reward used up");
-        IERC20(nmt).transfer(lf.owner, reward);
-        emit Claim(id, msg.sender, reward);
-
-        //update lockInfo
+        uint256 reward = lf.locked * rewardPropotion / 1000;
+        require(address(this).balance - reward >= totalLocked, "reward used up");
         lf.rewardsEarned = reward;
-        lf.rewardsClaimTime = block.timestamp;
+        payable(msg.sender).transfer(reward);
+
+        emit ClaimReward(id, msg.sender, reward);
     }
 }
