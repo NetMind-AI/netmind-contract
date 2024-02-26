3092b4353ca829b728337def08840555c30ae5db
routery <rout@protagonist-us.com>
2024-02-26 18:46:07 +0800

    upate

--- contracts/proxy/NetMindToken_proxy.sol
+++ contracts/proxy/NetMindToken_proxy.sol
@@ -3,11 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./proxy.sol";
 
-<<<<<<<< HEAD:contracts/proxy/NetMindToken_proxy.sol
 contract NetMindTokenProxy is baseProxy{
-========
-contract GovernorProxy is baseProxy{
->>>>>>>> dev:contracts/proxy/Governor_proxy.sol
        constructor(address impl) {
         _setAdmin(msg.sender);
         _setLogic(impl);
