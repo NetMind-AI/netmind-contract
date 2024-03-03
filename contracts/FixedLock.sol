b8cef7d80333c292dac0781abb95facf5f2563df
AronJeames <zm@protagonist-us.com>
2024-03-03 19:43:57 +0800

    update time check

--- contracts/FixedLock.sol
+++ contracts/FixedLock.sol
@@ -173,10 +173,10 @@ contract FixedLock is Initializable {
         require(lf.locked > 0, "lockId unexsit");
         require(lf.owner == msg.sender, "only owner can call");
         require(lf.rewardsEarned == 0, "already claim");
-        
+
 
         //claim reward
-        uint256 reward = lf.locked * rewardPropotion / 1000;
+        uint256 reward = checkReward(id);
         require(reward > 0, "zero reward");
         require(address(this).balance - reward >= totalLocked, "reward used up");
         lf.rewardsEarned = reward;
