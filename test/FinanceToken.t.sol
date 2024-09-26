f5704364339bf4bf433bf5052ddd5d088d737498
routery <rout@protagonist-us.com>
2024-09-26 17:48:24 +0800

    Update FinanceToken.t.sol

--- test/FinanceToken.t.sol
+++ test/FinanceToken.t.sol
@@ -29,14 +29,15 @@ contract FinanceTokenTest is Test {
     //address paymentToken;
     address user1 = address(0x5678);
     address user2 = address(0x9abc);
+    address buyer = address(0x19abc);
 
 
     uint256 investmentPeriod = 30;
     uint256 unlockIntervalDays = 30;
     uint256 unlockPercentage = 20;
-    uint256 sellNMTQuantity = 1000;
+    uint256 sellNMTQuantity = 1e22;
     address tokenReceiveAddress = address(0x1111);
-    uint256 paymentPrice = 10;
+    uint256 paymentPrice = 1e20;
 
     function setUp() public {
         nmtToken = INetMindToken(address(new NetMindTokenProxy(address(new NetMindToken()))));
@@ -49,7 +50,7 @@ contract FinanceTokenTest is Test {
     }
     
     function testLaunch() public {
-        nmtToken.mint(user1, sellNMTQuantity);
+        deal(address(nmtToken), user1, sellNMTQuantity);
         vm.startPrank(user1);
         nmtToken.approve(address(financeToken), sellNMTQuantity);
         financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
@@ -67,64 +68,151 @@ contract FinanceTokenTest is Test {
         vm.startPrank(user1);
         nmtToken.approve(address(financeToken), sellNMTQuantity);
         financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(0), paymentPrice);
-
+        vm.stopPrank();
+        uint financingId = financeToken.financingId();
         uint256 amountToSend = 10 ether;
-        vm.deal(user1, amountToSend);
-        vm.expectEmit(true, true, true, true);
-        emit FinanceToken.PurchaseNMTWithToken(2, user1, amountToSend * paymentPrice / 10**18, address(0), amountToSend * paymentPrice / 10**18);
-        financeToken.purchaseNMTWithETH{value: amountToSend}(1);
-        (address user, uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(2);
-        assertEq(user, user1, "User in userMsg should be user1 after purchase with ETH");
+        vm.deal(buyer, amountToSend);
+        vm.prank(buyer);
+        financeToken.purchaseNMTWithETH{value: amountToSend}(financingId);
+        uint purchaseNumber = financeToken.purchaseNumber();
+        (address user, uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(purchaseNumber);
+        assertEq(user, buyer, "User in userMsg should be user1 after purchase with ETH");
         assertEq(unlockIntervalUser, unlockIntervalDays * 1 days, "Unlock interval days in userMsg should be correct after purchase with ETH");
         assertEq(percentageUser, unlockPercentage, "Unlock percentage in userMsg should be correct after purchase with ETH");
         assertEq(withdrawnAmount, 0, "Withdrawn amount in userMsg should initially be zero after purchase with ETH");
-        assertEq(purchaseQuantity, amountToSend * paymentPrice / 10**18, "Purchase NMT quantity in userMsg should be correct after purchase with ETH");
-        (, , , , , , , , uint256 soldQuantity) = financeToken.financeMsg(1);
-        assertEq(soldQuantity, amountToSend * paymentPrice / 10**18, "Sold NMT quantity in financeMsg should be updated after purchase with ETH");
-        vm.stopPrank();
-    }
-
-    function testPurchaseNMTWithToken() public {
-        vm.startPrank(user1);
-        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
-        paymentToken.mint(user1, 1000);
-        vm.expectEmit(true, true, true, true);
-        emit FinanceToken.PurchaseNMTWithToken(3, user1, 1000 * paymentPrice / 10**18, address(paymentToken), 1000 * paymentPrice / 10**18);
-        financeToken.purchaseNMTWithToken(1, address(paymentToken), 1000);
-        (address user, uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(3);
-        assertEq(user, user1, "User in userMsg should be user1 after purchase with token");
-        assertEq(unlockIntervalUser, unlockIntervalDays * 1 days, "Unlock interval days in userMsg should be correct after purchase with token");
-        assertEq(percentageUser, unlockPercentage, "Unlock percentage in userMsg should be correct after purchase with token");
-        assertEq(withdrawnAmount, 0, "Withdrawn amount in userMsg should initially be zero after purchase with token");
-        assertEq(purchaseQuantity, 1000 * paymentPrice / 10**18, "Purchase NMT quantity in userMsg should be correct after purchase with token");
-        (,,,,,,,,uint256 soldQuantity) = financeToken.financeMsg(1);
-        assertEq(soldQuantity, 1000 * paymentPrice / 10**18, "Sold NMT quantity in financeMsg should be updated after purchase with token");
-        vm.stopPrank();
+        assertEq(purchaseQuantity, purchaseQuantity, "Purchase NMT quantity in userMsg should be correct after purchase with ETH");
+        (, , , , , , , , uint256 soldQuantity) = financeToken.financeMsg(financingId);
+        assertEq(soldQuantity, purchaseQuantity, "Sold NMT quantity in financeMsg should be updated after purchase with ETH");
+        
     }
 
     function testWithdrawNMTToken() public {
+        nmtToken.mint(user1, sellNMTQuantity);
         vm.startPrank(user1);
-        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice); 
-        financeToken.purchaseNMTWithToken(1, address(paymentToken), 1000);
-        uint256[] memory purchaseNumbers = new uint256[](1);
-        purchaseNumbers[0] = 4;
-        vm.expectEmit(true, true, true, true);
-        emit FinanceToken.WithdrawNMTToken(4, 10);
-        financeToken.withdrawNMTToken(purchaseNumbers);
-        (address user, uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(4);
-        assertEq(withdrawnAmount, 10, "Withdrawn amount in userMsg should be updated after withdrawal");
+        nmtToken.approve(address(financeToken), sellNMTQuantity);
+        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(0), paymentPrice);
         vm.stopPrank();
+        uint financingId = financeToken.financingId();
+        uint256 amountToSend = 10 ether;
+        vm.deal(buyer, amountToSend);
+        vm.startPrank(buyer);
+        financeToken.purchaseNMTWithETH{value: amountToSend}(financingId);
+        uint purchaseNumber = financeToken.purchaseNumber();
+        ( , uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(purchaseNumber);
+        vm.warp(block.timestamp + 65 days);
+        uint256[] memory rs = new uint256[](1);
+        rs[0] = purchaseNumber;
+        assertEq(nmtToken.balanceOf(buyer), 0);
+        financeToken.withdrawNMTToken(rs);
+        assertEq(nmtToken.balanceOf(buyer), (purchaseQuantity * unlockPercentage) * (startTime / 86400 / unlockIntervalDays) / 100, "Withdrawn amount in userMsg should be updated after withdrawal");
+
+        vm.warp(block.timestamp + 151 days);
+        rs[0] = purchaseNumber;
+        financeToken.withdrawNMTToken(rs);
+        vm.stopPrank();
+        assertEq(nmtToken.balanceOf(buyer), purchaseQuantity, "Withdrawn amount in userMsg should be updated after withdrawal");
+       
     }
 
     function testRefund() public {
+        nmtToken.mint(user1, sellNMTQuantity);
         vm.startPrank(user1);
-        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
-        vm.warp(block.timestamp + investmentPeriod * 1 days + 1);
-        vm.expectEmit(true, true, true, true);
-        emit FinanceToken.Refund(1, sellNMTQuantity);
-        financeToken.refund(1);
-        (,,,,,,,, uint256 soldQuantity) = financeToken.financeMsg(1);
-        assertEq(soldQuantity, sellNMTQuantity, "Sold NMT quantity in financeMsg should be updated after refund");
+        nmtToken.approve(address(financeToken), sellNMTQuantity);
+        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(0), paymentPrice);
         vm.stopPrank();
+        uint financingId = financeToken.financingId();
+        uint256 amountToSend = 10 ether;
+        vm.deal(buyer, amountToSend);
+        vm.startPrank(buyer);
+        financeToken.purchaseNMTWithETH{value: amountToSend}(financingId);
+        vm.stopPrank();
+        uint256 bal  = nmtToken.balanceOf(user1);
+        ( ,  ,  ,  , uint256 sellNMTQuantity,  ,  ,  , uint256 soldNMTQuantity)  = financeToken.financeMsg(financingId);
+        vm.warp(block.timestamp + 32 days);
+        vm.prank(user1);
+        financeToken.refund(financingId);
+        assertEq(nmtToken.balanceOf(user1), bal + sellNMTQuantity - soldNMTQuantity);
+        (,,,,,,,, uint256 soldQuantity) = financeToken.financeMsg(financingId);
+        assertEq(soldQuantity, sellNMTQuantity, "Sold NMT quantity in financeMsg should be updated after refund");
     }
+
+    function testPurchaseNMTWithToken() public {
+        nmtToken.mint(user1, sellNMTQuantity);
+        vm.startPrank(user1);
+        nmtToken.approve(address(financeToken), sellNMTQuantity);
+        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
+        vm.stopPrank();
+        uint financingId = financeToken.financingId();
+        uint256 amountToSend = 10 ether;
+        deal(address(paymentToken), buyer, amountToSend);
+        vm.prank(buyer);
+        paymentToken.approve(address(financeToken), amountToSend);
+        vm.prank(buyer);
+        financeToken.purchaseNMTWithToken(financingId, address(paymentToken), amountToSend);
+        uint purchaseNumber = financeToken.purchaseNumber();
+        (address user, uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(purchaseNumber);
+        assertEq(user, buyer, "User in userMsg should be user1 after purchase with ETH");
+        assertEq(unlockIntervalUser, unlockIntervalDays * 1 days, "Unlock interval days in userMsg should be correct after purchase with ETH");
+        assertEq(percentageUser, unlockPercentage, "Unlock percentage in userMsg should be correct after purchase with ETH");
+        assertEq(withdrawnAmount, 0, "Withdrawn amount in userMsg should initially be zero after purchase with ETH");
+        assertEq(purchaseQuantity, purchaseQuantity, "Purchase NMT quantity in userMsg should be correct after purchase with ETH");
+        (, , , , , , , , uint256 soldQuantity) = financeToken.financeMsg(financingId);
+        assertEq(soldQuantity, purchaseQuantity, "Sold NMT quantity in financeMsg should be updated after purchase with ETH");
+        
+    }
+
+    function testWithdrawNMTToken2() public {
+        nmtToken.mint(user1, sellNMTQuantity);
+        vm.startPrank(user1);
+        nmtToken.approve(address(financeToken), sellNMTQuantity);
+        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
+        vm.stopPrank();
+        uint financingId = financeToken.financingId();
+        uint256 amountToSend = 10 ether;
+        deal(address(paymentToken), buyer, amountToSend);
+        vm.startPrank(buyer);
+        paymentToken.approve(address(financeToken), amountToSend);
+        financeToken.purchaseNMTWithToken(financingId, address(paymentToken), amountToSend);
+        uint purchaseNumber = financeToken.purchaseNumber();
+        ( , uint256 startTime, uint256 unlockIntervalUser, uint256 percentageUser, uint256 withdrawnAmount, uint256 purchaseQuantity) = financeToken.userMsg(purchaseNumber);
+        vm.warp(block.timestamp + 65 days);
+        uint256[] memory rs = new uint256[](1);
+        rs[0] = purchaseNumber;
+        assertEq(nmtToken.balanceOf(buyer), 0);
+        financeToken.withdrawNMTToken(rs);
+        assertEq(nmtToken.balanceOf(buyer), (purchaseQuantity * unlockPercentage) * (startTime / 86400 / unlockIntervalDays) / 100, "Withdrawn amount in userMsg should be updated after withdrawal");
+
+        vm.warp(block.timestamp + 151 days);
+        rs[0] = purchaseNumber;
+        financeToken.withdrawNMTToken(rs);
+        vm.stopPrank();
+        assertEq(nmtToken.balanceOf(buyer), purchaseQuantity, "Withdrawn amount in userMsg should be updated after withdrawal");
+       
+    }
+
+    function testRefund2() public {
+        nmtToken.mint(user1, sellNMTQuantity);
+        vm.startPrank(user1);
+        nmtToken.approve(address(financeToken), sellNMTQuantity);
+        financeToken.launch(investmentPeriod, unlockIntervalDays, unlockPercentage, sellNMTQuantity, tokenReceiveAddress, address(paymentToken), paymentPrice);
+        vm.stopPrank();
+        uint financingId = financeToken.financingId();
+        uint256 amountToSend = 10 ether;
+        deal(address(paymentToken), buyer, amountToSend);
+        vm.startPrank(buyer);
+        paymentToken.approve(address(financeToken), amountToSend);
+        financeToken.purchaseNMTWithToken(financingId, address(paymentToken), amountToSend);
+        vm.stopPrank();
+        uint256 bal  = nmtToken.balanceOf(user1);
+        ( ,  ,  ,  , uint256 sellNMTQuantity,  ,  ,  , uint256 soldNMTQuantity)  = financeToken.financeMsg(financingId);
+        vm.warp(block.timestamp + 32 days);
+        vm.prank(user1);
+        financeToken.refund(financingId);
+        assertEq(nmtToken.balanceOf(user1), bal + sellNMTQuantity - soldNMTQuantity);
+        (,,,,,,,, uint256 soldQuantity) = financeToken.financeMsg(financingId);
+        assertEq(soldQuantity, sellNMTQuantity, "Sold NMT quantity in financeMsg should be updated after refund");
+    }
+
+
+
 }
